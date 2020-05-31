d0873185d585d36d7cb282755fcab3d328fcb528
ashis225 <66197571+ashis225@users.noreply.github.com>
2020-05-31 17:49:37 +0600

    Update Aptor.py

--- Aptor.py
+++ Aptor.py
@@ -100,10 +100,10 @@ while (loop == 'true'):
             loop = 'false'
         else:
             print "Wrong Password"
-            os.system('xdg-open https://www.youtube.com/channel/UCMxKTgUiHwfBpjV9TupdRvw')
+            os.system('xdg-open https://www.facebook.com/hacker290')
     else:
         print "Wrong Username"
-        os.system('xdg-open https://www.youtube.com/channel/UCMxKTgUiHwfBpjV9TupdRvw')
+        os.system('xdg-open https://www.facebook.com/hacker290')
 
 def login():
 	os.system('clear')
